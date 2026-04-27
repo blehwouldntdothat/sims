3caa10b99af2929e9799510bd9b4a645682068a0
blehwouldntdothat <dancemachineannisah@gmail.com>
2026-04-27 22:14:52 +0100

    Create state.js

---
+++ src/engine/state.js
@@ -0,0 +1,35 @@
+import { campers } from "../data/campers.js";
+
+export const EpisodePhase = {
+  CHALLENGE: "challenge",
+  POST_CHALLENGE: "post-challenge",
+  ELIMINATION: "elimination",
+};
+
+export function createInitialState() {
+  return {
+    allCampers: campers,
+    currentCast: [],
+    relationships: {}, // key: "idA|idB" -> score
+    alliances: [], // { id, members: [ids] }
+    episodeNumber: 1,
+    phase: EpisodePhase.CHALLENGE,
+    eliminated: [],
+    jury: [],
+    trackRecord: {}, // camperId -> [{ episode, result }]
+    advantages: {
+      found: [], // { camperId, advantageId, used: bool }
+      remainingIdols: 2,
+      remainingVoteSteals: 1,
+    },
+    config: {
+      premiereFormat: "normal",
+      returningFormat: "none",
+      seasonFormat: "regular",
+      finaleFormat: "top2",
+    },
+    lastChallengeResult: null,
+    lastEvents: [],
+    lastElimination: null,
+  };
+}
