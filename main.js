604eb7b7437f07852b271c020ee3daa3c90cbfae
blehwouldntdothat <dancemachineannisah@gmail.com>
2026-04-28 17:06:17 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-import { initMenuUI } from "./src/ui/menu.js";
-import { initEpisodeView } from "./src/ui/episodeView.js";
-import { createInitialState } from "./src/engine/state.js";
+import { initMenu } from "./src/ui/menu.js";
 
-const appState = createInitialState();
-
-window.appState = appState; // handy for debugging in console
-
+// Start the UI once the page loads
 document.addEventListener("DOMContentLoaded", () => {
-  initMenuUI(appState);
-  initEpisodeView(appState);
+  initMenu();
 });
